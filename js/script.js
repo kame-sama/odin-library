78bc0d60699f52b5248e08bcb4a1b05f3b1bf27c
kame-sama <ad.bodeev@gmail.com>
2023-12-08 12:48:30 +0500

    Add layouts and icons

--- js/script.js
+++ js/script.js
@@ -10,6 +10,30 @@ const library = [
     author: 'John R. R. Tolkien',
     pages: '318',
     read: 'not read'
+  },
+  {
+    title: 'The Stranger',
+    author: 'Alber Camus',
+    pages: '85',
+    read: 'read'
+  },
+  {
+    title: 'The Plague',
+    author: 'Alber Camus',
+    pages: '200',
+    read: 'not read'
+  },
+  {
+    title: 'The Fall',
+    author: 'Alber Camus',
+    pages: '98',
+    read: 'not read'
+  },
+  {
+    title: 'The Lord of the Rings',
+    author: 'John R. R. Tolkien',
+    pages: '860',
+    read: 'read'
   }
 ];
 
@@ -18,7 +42,7 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#status');
 
-const newBookButton = document.querySelector('.header button');
+const newBookButton = document.querySelector('.new-book button');
 const newBookDialog = document.querySelector('dialog');
 const submitButton = document.querySelector('#submit');
 const closeDialogButton = document.querySelector('dialog > button');
@@ -97,10 +121,16 @@ function populateCard(book, index) {
   readContent.textContent = book.read;
 
   const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete');
   deleteButton.textContent = 'Delete';
 
   const statusButton = document.createElement('button');
-  statusButton.textContent = 'Change status';
+  const statusIcon = document.createElement('span');
+  statusIcon.classList.add('material-symbols-outlined', 'size40');
+  statusIcon.textContent = book.read == 'read' 
+    ? 'toggle_on'
+    : 'toggle_off';
+  statusButton.appendChild(statusIcon);
 
 
   card.appendChild(titleHeading);
@@ -112,8 +142,8 @@ function populateCard(book, index) {
   card.appendChild(readHeading);
   card.appendChild(readContent);
 
-  card.appendChild(deleteButton);
   card.appendChild(statusButton);
+  card.appendChild(deleteButton);
 
   container.appendChild(card);
 
@@ -123,14 +153,18 @@ function populateCard(book, index) {
 }
 
 function changeReadStatus(event) {
-  const card = event.target.parentNode;
+  const card = event.target.localName == 'span' 
+  ? event.target.parentNode.parentNode
+  : event.target.parentNode;
   const index = card.getAttribute('data-index');
   const status = card.querySelector('p:last-of-type');
 
   if (status.textContent == 'read') {
     status.textContent = 'not read';
+    event.target.textContent = 'toggle_off';
   } else {
     status.textContent = 'read';
+    event.target.textContent = 'toggle_on';
   }
 
   library[index].read = status.textContent;
