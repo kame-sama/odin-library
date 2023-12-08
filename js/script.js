f7fdd946aa190184647206be0520a082d2de1913
kame-sama <ad.bodeev@gmail.com>
2023-12-08 08:18:55 +0500

    Add delete and change status buttons

--- js/script.js
+++ js/script.js
@@ -18,17 +18,17 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#status');
 
-populateDisplay(library);
+const newBookButton = document.querySelector('.header button');
+const newBookDialog = document.querySelector("dialog");
+const submitButton = document.querySelector("#submit")
 
-const addBtn = document.querySelector('.header button');
-const dialog = document.querySelector("dialog");
-const submitBtn = document.querySelector("#submit")
+populateDisplay();
 
-addBtn.addEventListener('click', () => {
-  dialog.show();
+newBookButton.addEventListener('click', () => {
+  newBookDialog.show();
 });
 
-submitBtn.addEventListener('click', getUserData);
+submitButton.addEventListener('click', processUserData);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -37,38 +37,41 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function getUserData(event) {
+function processUserData(event) {
   event.preventDefault();
-  addBookToLibrary();
-  populateDisplay(library);
-  clearInputs();
-  dialog.close();
+  if (title.value && author.value && pages.value && read.value) {
+    addBookToLibrary();
+    populateDisplay();
+    closeNewBookDialog();
+  }
 }
 
-function clearInputs() {
+function closeNewBookDialog() {
   title.value = '';
   author.value = '';
   pages.value = '';
   read.value = '';
+  newBookDialog.close();
 }
 
 function addBookToLibrary() {
   library.push(new Book(title.value, author.value, pages.value, read.value));
 }
 
-function populateDisplay(library) {
+function populateDisplay() {
   const cards = document.querySelectorAll('.card');
 
   for (let i = cards.length; i < library.length; i++) {
-    populateCard(library[i]);
+    populateCard(library[i], i);
   }
 }
 
-function populateCard(book) {
+function populateCard(book, index) {
   const container = document.querySelector('.container');
 
   const card = document.createElement('div');
   card.classList.toggle('card');
+  card.setAttribute('data-index', `${index}`);
 
   const titleHeading = document.createElement('h3');
   titleHeading.textContent = 'Title';
@@ -90,6 +93,13 @@ function populateCard(book) {
   const readContent = document.createElement('p');
   readContent.textContent = book.read;
 
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  const statusButton = document.createElement('button');
+  statusButton.textContent = 'Change status';
+
+
   card.appendChild(titleHeading);
   card.appendChild(titleContent);
   card.appendChild(authorHeading);
@@ -99,5 +109,43 @@ function populateCard(book) {
   card.appendChild(readHeading);
   card.appendChild(readContent);
 
+  card.appendChild(deleteButton);
+  card.appendChild(statusButton);
+
   container.appendChild(card);
+
+  deleteButton.addEventListener('click', deleteBookFromLibrary);
+
+  statusButton.addEventListener('click', changeReadStatus);
+}
+
+function changeReadStatus(event) {
+  const card = event.target.parentNode;
+  const index = card.getAttribute('data-index');
+  const status = card.querySelector('p:last-of-type');
+
+  if (status.textContent == 'read') {
+    status.textContent = 'not read';
+  } else {
+    status.textContent = 'read';
+  }
+
+  library[index].read = status.textContent;
+}
+
+function deleteBookFromLibrary(event) {
+  const card = event.target.parentNode;
+  const index = card.getAttribute('data-index');
+
+  library.splice(index, 1);
+  card.remove();
+  fixIndexOrder();
+}
+
+function fixIndexOrder() {
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach((card, index) => {
+    card.setAttribute('data-index', index);
+  })
 }
