a0f277a109702f06e9a85377eea210768d86e058
kame-sama <ad.bodeev@gmail.com>
2023-12-13 09:56:51 +0500

    Replace constructors with classes

--- js/script.js
+++ js/script.js
@@ -61,11 +61,13 @@ submitButton.addEventListener('click', processUserData);
 
 closeDialogButton.addEventListener('click', closeNewBookDialog);
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function processUserData(event) {
